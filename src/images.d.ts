39c6d5059215c53ef1628d4a13be1ff625ce231b
SD-card0402 <274192028+SD-card0402@users.noreply.github.com>
2026-07-18 11:16:18 +0900

    変更

--- src/images.d.ts
+++ src/images.d.ts
@@ -1,4 +1,5 @@
-declare module '*.png' {
-    const content: string;
-    export default content;
-}
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
