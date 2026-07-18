c718962aa87611b2628e38fb4fca075d9ba29699
SD-card0402 <274192028+SD-card0402@users.noreply.github.com>
2026-07-18 12:02:16 +0900

    tsファイルの削除

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -4,3 +4,9 @@ declare module '*.sass';
 declare module '*.module.css';
 declare module '*.module.scss';
 declare module '*.module.sass';
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
